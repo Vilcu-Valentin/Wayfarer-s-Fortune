44a2066ac97f31ebd79cd5a19c8a8878452f472e
Vilcu Valentin <114836678+Vilcu-Valentin@users.noreply.github.com>
2025-01-14 16:52:34 +0200

    Stuff

--- Assets/Scripts/CaravanCustomization/Locomotive.cs
+++ Assets/Scripts/CaravanCustomization/Locomotive.cs
@@ -8,8 +8,8 @@ public class Locomotive : CaravanBody
     // Start is called before the first frame update
     void Start()
     {
-        upgradesManager.UpdateVehicleUpgrades();
-        spacing = upgradesManager.GetCurrentFrame().spacing;
+        //upgradesManager.UpdateVehicleUpgrades();
+        //spacing = upgradesManager.GetCurrentFrame().spacing;
     }
 
 }
